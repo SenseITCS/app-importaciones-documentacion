47d8c99bdd749647aac22b550a9e001d3e99d9d1
SenseITCS <67979315+SenseITCS@users.noreply.github.com>
2025-01-27 15:06:18 -0600

    Agregar configuración de rutas

---
+++ src/frontend/importaciones-app/src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DocumentosComponent } from './components/documentos/documentos.component';
+import { PoPlanComponent } from './components/po-plan/po-plan.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { 
+    path: 'dashboard', 
+    component: DashboardComponent,
+    canActivate: [AuthGuard]
+  },
+  { 
+    path: 'documents', 
+    component: DocumentosComponent,
+    canActivate: [AuthGuard]
+  },
+  { 
+    path: 'po-plan', 
+    component: PoPlanComponent,
+    canActivate: [AuthGuard]
+  },
+  { path: '**', redirectTo: '/dashboard' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
