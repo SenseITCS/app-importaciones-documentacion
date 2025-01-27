4b43279bfe004f3f474a2f2be911112fc9250442
SenseITCS <67979315+SenseITCS@users.noreply.github.com>
2025-01-27 15:56:15 -0600

    Agregar modelo de compañía

---
+++ src/backend/ImportacionesApp.API/Models/Company.cs
@@ -0,0 +1,10 @@
+namespace ImportacionesApp.API.Models
+{
+    public class Company
+    {
+        public string ID { get; set; }
+        public string NAME { get; set; }
+        public string STATUSCOMPANY { get; set; }
+        public string COMPANYCLASSIFICATION { get; set; }
+    }
+}
